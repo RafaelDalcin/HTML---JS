fab6a859ebe4548ccf850c469d14101c3c2f945e
RafaelDalcin <rafael.dalcin@unochapeco.edu.br>
2022-06-20 18:18:42 -0300

    Aprendendo eventos e seletores JS e HTML

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    let aluno = prompt("Informe o nome do aluno");
+    if(!aluno){
+        return alert('É preciso informar o nome do aluno');
+    }
+let somaNotas = 0;
+let somaPesos = 0;
+let nota = 0;
+let peso = 0;
+let media = 0;
+while(confirm('Deseja lançar uma nota?')){
+    nota = Number(prompt('Informe a nota'));
+    peso = Number(prompt('Informe o peso'));
+    somaNotas += nota * peso;
+    somaPesos += peso;
+}
+if(somaPesos > 0){
+    media = somaNotas / somaPesos;
+    alert(`A média do aluno ${aluno} foi de ${media.toFixed(2)}`);
+}
+else{
+    alert('Nenhuma nota informada');
+}
+})
+
